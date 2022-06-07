e12b1ef657b15849f9fbbea9641c1510bdccf699
ignacio degregori <ignacio.degregori@inviu.com.ar>
2022-06-07 12:24:14 -0300

    project setup:
    -add modules
    -set main screen
    -set git
    -set ts

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MainScreen } from './screens';
+
+ReactDOM.render(
+	<MainScreen />,
+	document.getElementById('app-root'),
+);
---
+++ src/screens/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    justify-items: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+
+`;
+
+export const MainScreen = () => <Container>
+    
+</Container>;
